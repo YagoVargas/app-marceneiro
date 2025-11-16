2bc8c0aefd2dad0adc595b0fb9fdd68f89e223e4
YagoVargas <iagodevargas.idv@gmail.com>
2025-11-16 06:04:09 -0300

    Update client.ts

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,7 +1,10 @@
-import { createBrowserClient } from '@supabase/ssr';
+"use client";
+
+import { createBrowserClient } from "@supabase/ssr";
 
 export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
+
 
